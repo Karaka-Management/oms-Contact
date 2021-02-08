25769b5b0b658729a84d1da87983830520ba6b41
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Contact' => [
+    'Address' => '',
+    'Addresses' => '',
+    'City' => '',
+    'Contact' => '',
+    'Contacts' => '',
+    'Country' => '',
+    'Email' => '',
+    'FAO' => '',
+    'Fax' => '',
+    'Name' => '',
+    'Office' => '',
+    'Phone' => '',
+    'Private' => '',
+    'Province' => '',
+    'State' => '',
+    'Zip' => '',
+]];
